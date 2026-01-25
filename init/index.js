49e1cc6b6e94835a1d88483323da2303d7407cca
sayeda <you@example.com>
2026-01-25 19:56:32 +0530

    project

--- init/index.js
+++ init/index.js
@@ -1,21 +1,26 @@
-const mongoose=require('mongoose');
-const initdata=require('./data');
-const Listing=require('../models/listing');
-
-main().then(()=>{
-    console.log('Connected to MongoDB');
-}).catch(err => {
-    console.error('Error connecting to MongoDB:', err);
-});
+const mongoose = require("mongoose");
+const Listing = require("../models/listing");
+const User = require("../models/user");
+const data = require("./data");
+require("dotenv").config();
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+  await mongoose.connect(process.env.ATLASDB_URL);
+
+  // 👇 find your user
+  const user = await User.findOne({ username: "Fathema" });
+
+  // add owner to each listing
+  const listingsWithOwner = data.data.map(obj => ({
+    ...obj,
+    owner: user._id,
+  }));
+
+  await Listing.deleteMany({});
+  await Listing.insertMany(listingsWithOwner);
+
+  console.log("Seeded with owner");
+  mongoose.connection.close();
 }
 
-const initDB=async()=>{
-    await Listing.deleteMany({});
-    initdata.data.map((obj)=>({ ...obj,owner:"6487f3f4f0d5c4b1a4e8b123"}));
-    await Listing.insertMany(initdata.data);
-    console.log("Database initialized with sample data");
-};
-initDB();
+main();
